9c96938e1168360b1b8e01736143392676854a55
lydmgem <lydmgem@gmail.com>
2019-03-09 20:45:22 -0600

    Adding some starter code for server.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,3 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-
-CREATE TABLE burgers (
-id INT NOT NULL auto_increment,
-burger_name VARCHAR(30),
-devoured BOOLEAN DEFAULT true,
-);
-
 INSERT INTO burgers (burger_name, devoured)
 VALUES
 ();
--- server.js
+++ server.js
@@ -1,2 +1,30 @@
+// Dependencies
 require('dotenv').config();
 var express = require('express');
+var mysql = require('mysql');
+var exphbs = require("express-handlebars");
+
+// PORT setup for the application
+var PORT = process.env.PORT || 3306;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// Set up handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Set up MySQL
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASSWORD,
+        database: "movie_planner_db"
+      })
+};
